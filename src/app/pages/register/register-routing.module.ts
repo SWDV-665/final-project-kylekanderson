0a5856e4e0a65c8d47a3566ca98012af426b4008
Kyle Anderson <kylea@schwebdesign.com>
2020-11-14 09:43:59 -0500

    fixed registration redirect

--- src/app/pages/register/register-routing.module.ts
+++ src/app/pages/register/register-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
   {
     path: '',
     component: RegisterPage
+  },
+  {
+    path: 'home',
+    redirectTo: '/tabs/readings',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from  "@angular/router";
+import { Router } from "@angular/router";
 import { AuthenticationService } from '../../services/authentication.service';
+import { User, Chemicals } from '../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -9,15 +10,19 @@ import { AuthenticationService } from '../../services/authentication.service';
 })
 export class RegisterPage implements OnInit {
 
-  constructor(private  authService:  AuthenticationService, private  router:  Router) { }
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
   }
 
   register(form) {
+    let chemicals = new Chemicals(form.value.chlorine, form.value.ph_up, form.value.ph_down, form.value.alkalinity_up, form.value.alkalinity_down, form.value.calcium_up, form.value.calcium_down);
+    let user = new User(form.value.user_name, form.value.email, form.value.password, form.value.name, form.value.pool_gallons, form.value.pool_type, chemicals);
     console.log(form);
-    this.authService.register(form.value).subscribe((res) => {
-      this.router.navigateByUrl('home');
+    this.authService.register(user).subscribe((res) => {
+      this.authService.token = res._id;
+      this.authService.isAuthenticated.next(true);
+      this.router.navigateByUrl('/tabs', { replaceUrl: true });
     });
   }
 
